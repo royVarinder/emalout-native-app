47436760dff6bfb57e1ca95b2f27c775c8aabead
Garry <>
2024-12-27 19:45:19 +0530

    issues resolved

--- app/HomeScreen.jsx
+++ app/HomeScreen.jsx
@@ -1,36 +1,35 @@
 // HomeScreen.tsx
-import CustomHeader from '@/components/CustomHeader';
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import CustomHeader from "@/components/CustomHeader";
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
-    return (
-        <>
-            <View style={styles.container}>
-                <Text style={styles.title}>Welcome to the Home Screen!</Text>
-                <Button
-                    title="Go to Details"
-                    onPress={() => navigation.navigate('Details')} // Navigate to the Details screen
-                />
-            </View>
-            <CustomHeader />
-        </>
-
-    );
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome to the Home Screen!</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => navigation.navigate("Details")} // Navigate to the Details screen
+        />
+      </View>
+      <CustomHeader />
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#f5f5f5',
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 20,
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
 });
 
 export default HomeScreen;
